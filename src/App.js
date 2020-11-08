8f415ae178bb193eaef114d7302d13158b0958a6
ibizaSunrise <milalemekh@gmail.com>
2020-11-08 16:15:08 +0100

    Added Feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router} from 'react-router-dom'
 import { GlobalStyle } from './globalStyles';
 import { Hero } from './components/Hero'
 import  Products  from './components/Products';
+import  Feature  from './components/Feature';
 import { productData } from './components/Products/data';
 
 
@@ -14,6 +15,7 @@ function App() {
       <GlobalStyle/>
       <Hero/>
       <Products heading = 'Choose your favorite' data = {productData}/>
+      <Feature/>
     </Router>
   );
 }
